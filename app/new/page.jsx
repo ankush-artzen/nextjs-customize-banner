42bcb460480565b97ffd453b20af22a242ee6191
jchandan0001 <chandanjoshi002@gmail.com>
2025-07-03 12:51:15 +0530

    add new page

---
+++ app/new/page.jsx
@@ -0,0 +1,9 @@
+export default function NewPage() {
+    return (
+      <main className="p-6">
+        <h1 className="text-3xl font-bold mb-4">🚀 This is the New Page</h1>
+        <p className="text-lg text-gray-700">You can now route to /new in your Next.js app!</p>
+      </main>
+    );
+  }
+  
